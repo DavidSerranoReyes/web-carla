e13003ab5a636bbdc858013054d7fc7fa0963ed5
David Serrano <david-serrano@outlook.com>
2025-11-19 14:31:34 -0500

    fix: set cpanel domain configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
-// Detectar si está en producción por dominio personalizado
+// Detectar el entorno
+const isVercel = process.env.VERCEL === '1';
 const isDomainProduction =
   process.env.NODE_ENV === 'production' && process.env.VERCEL_URL?.includes('carlarreasanchez.com');
 
 export default defineConfig({
   integrations: [react(), sitemap()],
-  site: isDomainProduction
-    ? 'https://www.carlarreasanchez.com'
-    : 'https://davidserranoreyes.github.io',
-  base: isDomainProduction ? '/' : '/web-carla',
+  site: 'https://www.carlarreasanchez.com',
+  base: '/',
   trailingSlash: 'always',
   build: {
     format: 'directory',
